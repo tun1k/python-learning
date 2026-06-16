32ada1831c526bad5343a118b39ceb39bb54aa06
Thuta Tun <112250215+tun1k@users.noreply.github.com>
2026-06-16 21:40:17 +0630

    Add files via upload

---
+++ extending_list.py
@@ -0,0 +1,14 @@
+class SuperList(list):
+
+    def __len__(self):
+        return 1000
+    
+super_list1 = SuperList()
+print(len(super_list1))
+
+super_list1.append(5)
+print(super_list1[0])
+
+# checking
+print(issubclass(SuperList, list))
+print(issubclass(list, object))
